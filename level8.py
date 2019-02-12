951ea7d00ea9b9b623d739892161b8f666102219
rafaelgreca <rafaelgreca@unifei.edu.br>
2019-02-12 02:57:05 -0200

    Level 8

---
+++ level8.py
@@ -0,0 +1,15 @@
+#
+# github.com/rafaelgreca
+# Copyright © Rafael Greca Vieira
+#
+
+import bz2
+
+def decode_username_and_password(data):
+    return bz2.decompress(data).decode()
+
+un = b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+pw = b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+user = decode_username_and_password(un)
+password = decode_username_and_password(pw)
+print(user, password)
